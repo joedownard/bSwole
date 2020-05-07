421d25d1f3cbfc0e816edec7b9072d8f0aef5a91
BuildTools <finn.butler@warwick.ac.uk>
2020-05-08 00:35:12 +0100

    test commit

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ export default function App() {
       <View style={styles.header}>
         <Text style={styles.title}>bSwole</Text>
       </View>
-      <View style={styles.container}>
+      <View style={styles.content}>
         <Text style={styles.title}>content</Text>
       </View>
     </View>
@@ -15,16 +15,21 @@ export default function App() {
 }
 
 const styles = StyleSheet.create({
-  header: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  title: {
-    color: 'red',
-    fontStyle: 'bold',
-    fontFamily: 'Verdana',
-    fontSize: 30,
-  },
+    header: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+
+    title: {
+        color: 'red',
+        fontStyle: 'bold',
+        fontFamily: 'Verdana',
+        fontSize: 30,
+    },
+    content: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '50%',
+    },
 });
